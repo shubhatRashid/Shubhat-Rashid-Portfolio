9470255ee496a1c743865fa90febe60547e8de1e
shubhat33 <rashidshubhat41@gmail.com>
2023-08-18 12:26:55 +0530

    configured environment variables

--- src/client.js
+++ src/client.js
@@ -1,10 +1,11 @@
+
 // sanity.js
 import {createClient} from '@sanity/client'
 // Import using ESM URL imports in environments that supports it:
 // import {createClient} from 'https://esm.sh/@sanity/client'
 
 export const client = createClient({
-    projectId: 't2101w2m',
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
     dataset: 'production',
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
